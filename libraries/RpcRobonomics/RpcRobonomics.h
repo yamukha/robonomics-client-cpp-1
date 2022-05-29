e8d018c14c32e74713a9fa523c7dbaa9100ba805
Andriy <yamukha@ukr.net>
2022-05-29 19:40:07 +0300

    add TransferBalance

--- libraries/RpcRobonomics/RpcRobonomics.h
+++ libraries/RpcRobonomics/RpcRobonomics.h
@@ -21,6 +21,7 @@
     Data head = Data{0x33,0}; // call header for Datalog record + some payload
 #endif
 #endif 
+
 typedef struct {
    std::string body;      // responce body
    uint32_t code;         // http responce code 200, 404, 500 etc.
@@ -101,12 +102,112 @@ class RobonomicsRpc {
                   Serial.println("Try 2nd stage with extrinsic"); 
                   if (isGetParameters_) {
                     fj = parseJson (val);
-                    Data call = callDatalogRecord(head, record); // call header for Datalog record + some payload
+                    Data head_dr = Data{0x33,0};     // call header for Datalog record to remote server
+                    Data call = callDatalogRecord(head_dr, record); // call header for Datalog record + some payload
                     Data data_ = doPayload (call, fj.era, fj.nonce, fj.tip, fj.specVersion, fj.tx_version, fj.ghash, fj.bhash);
                     Data signature_ = doSign (data_, privateKey_, publicKey_);
                     std::vector<std::uint8_t> pubKey( reinterpret_cast<std::uint8_t*>(std::begin(publicKey_)), reinterpret_cast<std::uint8_t*>(std::end(publicKey_)));               
                     edata_ = doEncode (signature_, pubKey, fj.era, fj.nonce, fj.tip, call);
-                    Serial.printf("size %d\n", edata_.size()); 
+                    Serial.printf("extrinsic %s: size %d\n", "DatalogRecord", edata_.size());
+                    isGetParameters_ = false;
+                  } else {
+                    isGetParameters_ = true;
+                    RpcResult r {"O.K", httpCode};
+                    return r;
+                  }
+                } else {
+                  isGetParameters_ = true;
+                  RpcResult r {"htpp O.K. but RPC error ", httpCode};
+                  return r;
+               }// res_
+           } // json parse
+        } else {
+            isGetParameters_ = true;
+            RpcResult r {"http not 200 error: ", httpCode};
+            return r;
+         } // httpCode == HTTP_CODE_OK
+      } else {
+        isGetParameters_ = true;
+        RpcResult r {"http > 0 error: ", httpCode};
+        return r;
+      } // httpCode > 0
+    } // for
+    isGetParameters_ = true;
+    RpcResult r {"http: ", HTTP_CODE_OK};
+    return r; 
+  };
+  
+  RpcResult TransferBalance (std::string dst, uint64_t fee) {
+
+    Data edata_;
+    
+    for (int a = 0 ; a < 2;  a++) {
+
+      HTTPClient http;    
+      http.begin(wifi_, url_.c_str());
+      http.addHeader("Content-Type", "application/json");
+      Serial.print("[HTTP]+POST:\n"); 
+      JSONVar params; 
+      String jsonString;
+      if (isGetParameters_) {
+        jsonString = getPayloadJs (ss58adr_,id_counter_);
+      } else {
+        jsonString = fillParamsJs (edata_,id_counter_);
+        edata_.clear();
+      }
+      Serial.println("sent:");
+      Serial.println(jsonString);
+      id_counter_++;
+    
+      int httpCode = http.POST(jsonString);
+
+      if (httpCode > 0) {
+          Serial.printf("[HTTP]+POST code: %d\n", httpCode);
+            if (httpCode == HTTP_CODE_OK) {
+              const String& payload = http.getString();
+              Serial.println("received:");
+              Serial.println(payload);
+         
+              JSONVar myObject = JSON.parse(payload);
+              if (JSON.typeof(myObject) == "undefined") {
+                  Serial.println("");
+                  RpcResult r {"Parsing input failed!", -100};
+                  return r;               
+              } else {
+                // RPC FSM                 
+                JSONVar keys = myObject.keys();
+                bool res_ = false;
+                JSONVar val;
+                FromJson fj;
+                  
+                //"result" or "error" 
+                for (int i = 0; i < keys.length(); i++) { 
+                  JSONVar value = myObject[keys[i]];
+                  String str  = JSON.stringify (keys[i]);
+                 
+                  if(strstr(str.c_str(),"result")) {
+                    res_ = true;
+                    val = value;
+                  }
+       
+                  if(strstr(str.c_str(),"error"))  {
+                    val = value;
+                    isGetParameters_ = true;
+                  }
+                }
+                
+                // -- 2nd stage: create and send extrinsic
+                if (res_) {
+                  Serial.println("Try 2nd stage with extrinsic"); 
+                  if (isGetParameters_) {
+                    fj = parseJson (val);
+                    Data head_bt = Data{0x1f,0};  // call header for Balance transfer to remote server
+                    Data call = callTransferBalance(head_bt, dst, fee); // call header for Traansfer Balance + some payload
+                    Data data_ = doPayload (call, fj.era, fj.nonce, fj.tip, fj.specVersion, fj.tx_version, fj.ghash, fj.bhash);
+                    Data signature_ = doSign (data_, privateKey_, publicKey_);
+                    std::vector<std::uint8_t> pubKey( reinterpret_cast<std::uint8_t*>(std::begin(publicKey_)), reinterpret_cast<std::uint8_t*>(std::end(publicKey_)));               
+                    edata_ = doEncode (signature_, pubKey, fj.era, fj.nonce, fj.tip, call);
+                    Serial.printf("extrinsic %s: size %d\n", "TransferBalance", edata_.size());
                     isGetParameters_ = false;
                   } else {
                     isGetParameters_ = true;
--- rpcjsonc/rpcjsonc.ino
+++ rpcjsonc/rpcjsonc.ino
@@ -10,6 +10,8 @@
 // ss58 address and related to it ED25519 keypair, can be examined by subkey application or python script
 #define PRIV_KEY      "da3cf5b1e9144931a0f0db65664aab662673b099415a7f8121b7245fb0be4143"
 #define SS58_ADR       "5HhFH9GvwCST4kRVoFREE7qDJcjYteR5unhQCrBGhhGuRgNb"
+#define SS58_DST       "5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty"
+#define SS58_DST_KEY   "8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48"
 
 #ifndef STASSID
 #define STASSID "FLY-TL-WR741ND"
@@ -17,6 +19,7 @@
 #endif
 
 uint64_t id_counter = 0; 
+uint64_t coins_count = 0; 
 
 void setup() {
 
@@ -40,7 +43,9 @@ void loop () {
         WiFiClient client;
         Serial.println("RPC task run");
         RobonomicsRpc rpcProvider(client, URLRPC, PRIV_KEY, SS58_ADR, id_counter);
-        RpcResult r = rpcProvider.DatalogRecord(std::to_string(id_counter)); // id_counter as payload just for example
+        //RpcResult r = rpcProvider.DatalogRecord(std::to_string(id_counter)); // id_counter as payload just for example
+        RpcResult r = rpcProvider.TransferBalance(SS58_DST_KEY, coins_count); // coins_count as fee just for example
+        coins_count++;
         id_counter = id_counter + 2;
         Serial.printf("[RPC] %ld %s\n", r.code, r.body.c_str());  
         delay(1000);
